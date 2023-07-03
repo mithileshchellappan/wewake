f202f74159ce0f492819b1fa379eaa15b16e9408
mithileshchellappan <mithilesh153@gmail.com>
2023-07-03 15:25:38 +0530

    ADD:0.0.0.0 to ports

--- Program.cs
+++ Program.cs
@@ -68,5 +68,5 @@ app.MapGet("/ping", () => "pong");
 console.log(internalIp);
 app.Urls.Add($"http://{internalIp}:5022");
 app.Urls.Add("http://localhost:5022");
-//app.Urls.Add("http://0.0.0.0:80");
+app.Urls.Add("http://0.0.0.0:5022");
 app.Run();
