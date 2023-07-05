5da3df42bd0d2533e2ec6a7f40e30e1e018a9384
mithileshchellappan <mithilesh153@gmail.com>
2023-07-05 18:11:56 +0530

    INIT: Task model

--- lib/models/Task.dart
+++ lib/models/Task.dart
@@ -1,9 +1,36 @@
 class Task {
-  late String TaskId;
+  late String AlarmTaskId;
   late String AlarmId;
   late String TaskText;
   late bool IsDone;
-  bool IsNew;
+  late int DoneCount;
+  bool IsNew = false;
 
-  Task(this.AlarmId, this.TaskText, this.IsDone, {this.IsNew = false});
+  Task(this.AlarmId, this.TaskText, this.IsDone, this.DoneCount,
+      {this.IsNew = false});
+
+  Task.fromJson(dynamic json) {
+    AlarmTaskId = json["alarmTaskId"];
+    AlarmId = json["alarmId"];
+    TaskText = json["taskText"];
+    IsDone = json["isDone"];
+    DoneCount = json["doneCount"];
+    IsNew = false;
+  }
+
+  Map<String, dynamic> toJson(String groupId) => {
+        'GroupId': groupId,
+        'AlarmId': AlarmId,
+        'TaskText': TaskText,
+      };
+
+  static List<Task> fromListJson(List<dynamic> arr) {
+    List<Task> tasks = [];
+    for (var json in arr) {
+      Task task = Task.fromJson(json);
+      tasks.add(task);
+    }
+
+    return tasks;
+  }
 }
