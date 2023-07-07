fb856b5c1c06a7422856e1b924f620f6273207d5
Mithilesh Chellappan <35962266+mithileshchellappan@users.noreply.github.com>
2023-07-07 13:57:53 +0530

    MINOR

--- Program.cs
+++ Program.cs
@@ -71,7 +71,7 @@ app.UseWebSocketMiddleware("/ws");
 app.UseJWTMiddleware();
 app.MapGet("/ping", () => "pong");
 console.log(internalIp);
-app.Urls.Add($"http://{internalIp}:5022");
+//app.Urls.Add($"http://{internalIp}:5022");
 app.Urls.Add("http://localhost:5022");
 app.Urls.Add("http://0.0.0.0:5022");
 app.Run();
